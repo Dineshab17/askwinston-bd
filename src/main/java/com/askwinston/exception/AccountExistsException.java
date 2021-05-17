05ef2920375144fe7dafff44c6af69c606a79bbb
Ramya <ramya.v@pentabay.com>
2021-05-17 19:27:35 +0530

    Custom exception for account exists-2

--- src/main/java/com/askwinston/exception/AccountExistsException.java
+++ src/main/java/com/askwinston/exception/AccountExistsException.java
@@ -1,9 +1,11 @@
 package com.askwinston.exception;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.CONFLICT, reason="Account Already Exist")
+@Data
 public class AccountExistsException extends RuntimeException{
     private String email;
     public AccountExistsException(String message, String email){
